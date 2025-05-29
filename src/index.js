e4c9b0c50786562f8ba6715be9417a04b03b668e
eustaquio <eustaquio.galano@gmail.com>
2025-05-29 13:31:46 +0800

    feat: add event initializer for input validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import "./styles.css";
+import './styles.css';
+import { attachValidation } from './modules/events';
+
+// Initialize the attachment of validations on each input
+attachValidation();
