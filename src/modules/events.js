f42f9cc294b8a855eec4bc7cf32eb8a3417bde2f
eustaquio <eustaquio.galano@gmail.com>
2025-05-29 16:34:50 +0800

    feat: add event for validating country input

--- src/modules/events.js
+++ src/modules/events.js
@@ -2,9 +2,15 @@ import { validateInputs } from './validation';
 
 function attachValidation() {
   const validator = validateInputs();
+
+  // Get a reference of email input and put input event
   document
     .querySelector('#mail')
     .addEventListener('input', validator.emailValidator.validateEmail);
+
+  document
+    .querySelector('#country')
+    .addEventListener('input', validator.countryValidator.validateCountry);
 }
 
 export { attachValidation };
